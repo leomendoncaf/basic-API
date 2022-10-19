0b122f108584182dbf29fdda079a4dae4596b654
Iago <iago.luiz@ges.inatel.br>
2022-10-19 17:07:10 -0300

    :test_tube: add mock test

--- test/user_mock2.test.js
+++ test/user_mock2.test.js
@@ -27,3 +27,92 @@ describe('create', () => {
         );
     });
 });
+
+describe('editUser', () => {
+    it('Valid edit', async () => {
+        UserModel.findOneAndUpdate = jest.fn().mockImplementation(() => ({
+            exec: () => ({
+                toObject: () => ({
+                    id: 1,
+                    senha: '123Maria',
+                    email: 'jao10@email.com',
+                    nome: 'Maria João',
+                }),
+            }),
+        }));
+
+        expect(await user.update({
+            email: 'jao10@email.com',
+            nome: 'Maria João',
+        })).toEqual(
+            expect.objectContaining({
+                email: expect.any(String),
+                nome: expect.any(String),
+                senha: expect.any(String),
+                id: expect.any(Number),
+            }),
+        );
+    });
+});
+
+describe('listUsers', () => {
+    it('Valid list', async () => {
+        UserModel.find = jest.fn().mockImplementation(() => ({
+            exec: () => ({
+                id: 1,
+                email: 'cardozo10@email.com',
+                nome: 'André Cardozo',
+                senha: 'teste1234',
+            }),
+        }));
+
+        expect(await user.list()).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                email: 'cardozo10@email.com',
+                nome: 'André Cardozo',
+                senha: 'teste1234',
+            }),
+        );
+
+    });
+});
+
+describe('getByEmail', () => {
+    it('Valid list', async () => {
+        UserModel.findOne = jest.fn().mockImplementation(() => ({
+            exec: () => ({
+                id: 1,
+                email: 'cardozo10@email.com',
+                nome: 'André Cardozo',
+                senha: 'teste1234',
+            }),
+        }));
+
+        expect(await user.getByEmail({
+            email: 'cardozo10@email.com'
+        })).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                email: 'cardozo10@email.com',
+                nome: 'André Cardozo',
+                senha: 'teste1234',
+            }),
+        );
+
+    });
+});
+
+describe('deleteUser', () => {
+    it('Valid delete', async () => {
+        UserModel.deleteOne = jest.fn().mockImplementation(() => ({
+            exec: () => ({
+                deletedCount: 1,
+            }),
+        }));
+
+        expect(await user.delete({
+            email: 'jao10@email.com',
+        })).toEqual(1);
+    });
+});
